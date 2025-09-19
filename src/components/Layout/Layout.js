4b9810da5e4a57b6297e20f59241d573775ebd26
kikimora_intelligencd <naginathora78@gmail.com>
2025-09-19 17:42:34 +0300

    chore: add routing and layout components

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import "./Layout.css";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="layout-main">{children}</main>
+    </>
+  );
+};
+
+export default Layout;
